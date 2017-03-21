89543f8a0c423cc0d426b49172859036b5013009
kortex <jfcontart@idemobi.com>
2017-03-21 08:56:41 +0100

    init 2

--- TestGit/TestGit/ViewController.swift
+++ TestGit/TestGit/ViewController.swift
@@ -9,20 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var mTitleLabel : UILabel?
+    
+    @IBOutlet var mSegmentedBar : UISegmentedControl?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        mTitleLabel?.text = "Jean-François CONTART"
+        
+        ChangeColor ()
         
         //ghgfhfhg
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func IAmRich ()
+    {
+        mTitleLabel?.text = "0€"
+    }
+    
+    @IBAction func ChangeColor ()
+    {
+        NSLog(" Yahoo !!!!!")
+        if (mSegmentedBar?.selectedSegmentIndex == 0)
+        {
+            mTitleLabel?.textColor = #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)
+        }
+        else if (mSegmentedBar?.selectedSegmentIndex == 1)
+        {
+            mTitleLabel?.textColor = UIColor.init(red: 1.0, green: 0, blue: 0, alpha: 1)
+        }
+        else
+        {
+            mTitleLabel?.textColor = UIColor.init(red: 0.0, green: 1.0, blue: 0, alpha: 1)
+        }
+    }
+    
 }
 
